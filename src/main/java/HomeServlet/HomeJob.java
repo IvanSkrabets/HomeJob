3e6a1fbca024c3bc363badb7cdcbeec3b5ae0e46
we100ka@gmail.com <we100ka@yandex.by>
2022-05-21 22:43:24 +0300

    Initial commit

---
+++ src/main/java/HomeServlet/HomeJob.java
@@ -0,0 +1,19 @@
+package HomeServlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(urlPatterns = "/hello")
+public class HomeJob extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String name = req.getParameter("name");
+        String age = req.getParameter("age");
+        resp.getWriter().println("<h1>Hello" + name + age +  "</h1>");
+    }
+}
